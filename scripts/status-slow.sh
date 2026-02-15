ac19329513f600822c87cec6ad41fc59bb834855
Jack McGinty <jtmcginty16@gmail.com>
2026-02-14 22:37:04 -0600

    fix: properly integrate pomodoro status in status bar
    - Add #{pomodoro_status} interpolation variable to tmux.conf
    - Apply Catppuccin yellow color (#f9e2af) to pomodoro
    - Add proper separator spacing
    - Remove incorrect @pomodoro_status variable from scripts

--- scripts/status-slow.sh
+++ scripts/status-slow.sh
@@ -3,6 +3,5 @@
 
 weather=$(~/tmux-config/scripts/weather-safe.sh)
 battery=$(~/tmux-config/scripts/battery-color.sh)
-pomodoro=$(tmux show-option -gqv "@pomodoro_status")
 
-echo "#[fg=#f9e2af]$pomodoro#[fg=#89dceb]$weather #[fg=#6e6a86]| $battery"
+echo "#[fg=#89dceb]$weather #[fg=#6e6a86]| $battery"
